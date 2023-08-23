9836a87e90d4f94c791dad01ef254b53e5c41ed9
Lee HuiGwang <104819764+winteryy@users.noreply.github.com>
2023-08-24 03:37:21 +0900

    part2 ch01

---
+++ part2/chapter1/app/src/main/java/winterry/part2/chapter1/MainActivity.kt
@@ -0,0 +1,20 @@
+package winterry.part2.chapter1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.webkit.WebView
+import android.webkit.WebViewClient
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val webView = findViewById<WebView>(R.id.webView)
+        webView.webViewClient = WebViewClient()
+        webView.settings.javaScriptEnabled = true
+
+        webView.loadUrl("https://google.com")
+    }
+}
