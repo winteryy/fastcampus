4bd3de341f841086c66101a3ed030983e7bc4740
Lee HuiGwang <104819764+winteryy@users.noreply.github.com>
2023-08-27 03:46:55 +0900

    part2 ch03

--- part2/chapter3/app/src/main/java/winterry/part2/chapter3/MainActivity.kt
+++ part2/chapter3/app/src/main/java/winterry/part2/chapter3/MainActivity.kt
@@ -2,7 +2,14 @@ package winterry.part2.chapter3
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+import android.widget.Toast
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.io.PrintWriter
 import java.net.HttpURLConnection
+import java.net.Socket
 import java.net.URL
 
 
@@ -11,6 +18,32 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        Thread {
+            try {
+                val port = 8080
+                val socket = Socket("192.168.219.105", port)
+                val printer = PrintWriter(socket.getOutputStream())
+                val reader = BufferedReader(InputStreamReader(socket.getInputStream()))
+
+                printer.println("GET / HTTP/1.1")
+                printer.println("Host: 192.168.219.105:8080")
+                printer.println("User-Agent: android")
+                printer.println("\r\n")
+                printer.flush()
+
+                var input: String? = "-1"
+                while (input != null) {
+                    input = reader.readLine()
+                    Toast.makeText(this, input, Toast.LENGTH_SHORT).show()
+                    Log.d("Client", input)
+                }
+                reader.close()
+                printer.close()
+                socket.close()
+            } catch (e: Exception) {
+                Log.e("Client", "$e")
+            }
+        }.start()
     }
 
 }
