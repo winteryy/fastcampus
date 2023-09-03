e9a6dfabd60a423e6e9d2f8b32137e9fd14da3df
Lee HuiGwang <104819764+winteryy@users.noreply.github.com>
2023-09-04 04:24:31 +0900

    part2 ch07

---
+++ part2/chapter7/app/src/main/java/winterry/part2/chapter7/Forecast.kt
@@ -0,0 +1,11 @@
+package winterry.part2.chapter7
+
+data class Forecast(
+    val forecastDate: String,
+    val forecastTime: String,
+
+    var temperature: Double = 0.0,
+    var sky: String = "",
+    var precipitation: Int = 0,
+    var precipitationType: String = "",
+)
