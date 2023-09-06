ca90d15f852ad9436e4f231b232324278416252a
Lee HuiGwang <104819764+winteryy@users.noreply.github.com>
2023-09-07 02:43:06 +0900

    part2 ch08

---
+++ part2/chapter8/app/src/main/java/winterry/part2/chapter8/MainActivity.kt
@@ -0,0 +1,71 @@
+package winterry.part2.chapter8
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.naver.maps.geometry.LatLng
+import com.naver.maps.map.CameraAnimation
+import com.naver.maps.map.CameraUpdate
+import com.naver.maps.map.NaverMap
+import com.naver.maps.map.OnMapReadyCallback
+import winterry.part2.chapter8.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var naverMap: NaverMap
+    private var isMapInit = false
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.mapView.onCreate(savedInstanceState)
+
+        binding.mapView.getMapAsync(this)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        binding.mapView.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        binding.mapView.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        binding.mapView.onPause()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        binding.mapView.onStop()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding.mapView.onDestroy()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        binding.mapView.onSaveInstanceState(outState)
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        binding.mapView.onLowMemory()
+    }
+
+    override fun onMapReady(mapObject: NaverMap) {
+        naverMap = mapObject
+        isMapInit = true
+
+        val cameraUpdate = CameraUpdate.scrollTo(LatLng(37.5666102, 126.9783881))
+            .animate(CameraAnimation.Easing)
+        naverMap.moveCamera(cameraUpdate)
+    }
+}
