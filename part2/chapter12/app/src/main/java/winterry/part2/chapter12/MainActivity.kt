fa77d07b8ba81dae4daa63af664c768c6ab1c4e3
Lee HuiGwang <104819764+winteryy@users.noreply.github.com>
2023-09-21 03:57:41 +0900

    part2 ch12

--- part2/chapter12/app/src/main/java/winterry/part2/chapter12/MainActivity.kt
+++ part2/chapter12/app/src/main/java/winterry/part2/chapter12/MainActivity.kt
@@ -2,10 +2,31 @@ package winterry.part2.chapter12
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import winterry.part2.chapter12.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    private lateinit var videoAdapter: VideoAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        videoAdapter = VideoAdapter(this)
+
+        binding.videoListRecyclerView.apply {
+            layoutManager = LinearLayoutManager(context)
+            adapter = videoAdapter
+        }
+
+        val videoList = readData("videos.json", VideoList::class.java) ?: VideoList(emptyList())
+        videoAdapter.submitList(videoList.videos)
+
+        binding.playerRecyclerView
     }
 }
---
+++ part2/chapter12/app/src/main/java/winterry/part2/chapter12/Util.kt
@@ -0,0 +1,25 @@
+package winterry.part2.chapter12
+
+import android.content.Context
+import com.google.gson.Gson
+import com.google.gson.JsonSyntaxException
+import java.io.IOException
+
+fun <T> Context.readData(fileName: String, classT: Class<T>): T? {
+    return try {
+        val inputStream = this.resources.assets.open(fileName)
+        val byteArray = ByteArray(inputStream.available())
+        inputStream.read(byteArray)
+        inputStream.close()
+
+        val gson = Gson()
+        gson.fromJson(String(byteArray), classT)
+
+    } catch (e: IOException) {
+        e.printStackTrace()
+        null
+    } catch (e: JsonSyntaxException) {
+        e.printStackTrace()
+        null
+    }
+}
