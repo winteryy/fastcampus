51ba7906155080d2f3b1d095433f7a0e29ffb386
Lee HuiGwang <104819764+winteryy@users.noreply.github.com>
2024-01-11 03:38:02 +0900

    part4 ch04

--- part4/part4-chapter4/part4-chapter4-6/app/src/main/java/kr/co/fastcampus/part4/chapter4_6/MainActivity.kt
+++ part4/part4-chapter4/part4-chapter4-6/app/src/main/java/kr/co/fastcampus/part4/chapter4_6/MainActivity.kt
@@ -18,6 +18,7 @@ import androidx.compose.ui.graphics.painter.ColorPainter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ChainStyle
 import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.constraintlayout.compose.Dimension
 import coil.compose.AsyncImage
@@ -53,13 +54,11 @@ fun CardEx(cardData: CardData) {
         modifier = Modifier.padding(4.dp),
     ) {
         // 단계 1: 아래의 Row 레이아웃을 ConstraintLayout로 바꾸어 봅시다.
-        ConstraintLayout(modifier = Modifier.fillMaxWidth()) {
-        }
-
-        Row(
-            verticalAlignment = Alignment.CenterVertically,
-            modifier = Modifier.padding(8.dp)
+        ConstraintLayout(
+            modifier = Modifier
+                .fillMaxWidth()
         ) {
+            val (profileImage, author, description) = createRefs()
             AsyncImage(
                 model = cardData.imageUri,
                 contentDescription = cardData.imageDescription,
@@ -68,13 +67,69 @@ fun CardEx(cardData: CardData) {
                 modifier = Modifier
                     .clip(CircleShape)
                     .size(40.dp)
+                    .constrainAs(profileImage) {
+                        centerVerticallyTo(parent)
+                        start.linkTo(parent.start, margin = 8.dp)
+//                        linkTo(parent.top, parent.bottom)
+//                        top.linkTo(parent.top)
+//                        bottom.linkTo(parent.bottom)
+                    }
             )
-            Spacer(modifier = Modifier.size(8.dp))
-            Column {
-                Text(text = cardData.author)
-                Text(text = cardData.description)
+            Text(
+                text = cardData.author,
+                modifier = Modifier.constrainAs(author) {
+                    linkTo(
+                        profileImage.end,
+                        parent.end,
+                        startMargin = 8.dp,
+                        endMargin = 8.dp
+                    )
+                    width = Dimension.fillToConstraints
+                }
+            )
+            Text(
+                text = cardData.description,
+                modifier = Modifier.constrainAs(description) {
+                    linkTo(
+                        profileImage.end,
+                        parent.end,
+                        startMargin = 8.dp,
+                        endMargin = 8.dp
+                    )
+                    width = Dimension.fillToConstraints
+                }
+            )
+
+            val chain = createVerticalChain(
+                author,
+                description,
+                chainStyle = ChainStyle.Packed)
+
+            constrain(chain) {
+                top.linkTo(parent.top, margin = 8.dp)
+                bottom.linkTo(parent.bottom, margin = 8.dp)
             }
         }
+
+//        Row(
+//            verticalAlignment = Alignment.CenterVertically,
+//            modifier = Modifier.padding(8.dp)
+//        ) {
+//            AsyncImage(
+//                model = cardData.imageUri,
+//                contentDescription = cardData.imageDescription,
+//                contentScale = ContentScale.Crop,
+//                placeholder = ColorPainter(color = placeHolderColor),
+//                modifier = Modifier
+//                    .clip(CircleShape)
+//                    .size(40.dp)
+//            )
+//            Spacer(modifier = Modifier.size(8.dp))
+//            Column {
+//                Text(text = cardData.author)
+//                Text(text = cardData.description)
+//            }
+//        }
     }
 }
 
