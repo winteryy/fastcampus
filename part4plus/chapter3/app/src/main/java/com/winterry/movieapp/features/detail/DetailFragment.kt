2539bf5451c14ebba2700284b0c3d97ae9fa81fc
Lee HuiGwang <104819764+winteryy@users.noreply.github.com>
2024-01-19 02:45:19 +0900

    part4plus ch03

---
+++ part4plus/chapter3/app/src/main/java/com/winterry/movieapp/features/detail/DetailFragment.kt
@@ -0,0 +1,28 @@
+package com.winterry.movieapp.features.detail
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.compose.material3.Text
+import androidx.compose.ui.platform.ComposeView
+import androidx.fragment.app.Fragment
+import com.winterry.movieapp.ui.theme.MovieAppTheme
+
+class DetailFragment: Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return ComposeView(requireContext()).apply {
+            setContent {
+                MovieAppTheme {
+                    Text("DetailFragment")
+                }
+            }
+        }
+    }
+
+}
